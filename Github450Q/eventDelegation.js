e46ebc6f54d3c9e33f12a09da44d0d911ffb2a1b
Preet Kaler <preetsingh02064@gmail.com>
2025-12-19 22:07:21 +0530

    ok

---
+++ Github450Q/eventDelegation.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('ul');
+
+{/* <ul>
+    <li id="1"></li>
+    <li id="2"></li>
+    <li id="3"></li>
+</ul> */}
+
+container.addEventListener("click",(e)=>{
+    if(e.target.tagName === "LI"){           //tagName html ka tag return krta hai uppercase me
+        console.log(`li ${FileList.dataset.id} is clicked`);  
+    }
+})
+
+{/* <div>
+    <button id="1"></button>
+    <button id="1"></button>
+    <button id="1"></button>
+</div> */}
+
+const con = document.querySelector('div')
+con.addEventListener("click",(e)=>{
+    const button = e.target.closest("botton")
+    console.log(button.dataset.id);
+    
+})
