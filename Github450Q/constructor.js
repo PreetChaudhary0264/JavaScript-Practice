d436170553133e41ceaee8212628e1d2f6e3494e
Preet Kaler <preetsingh02064@gmail.com>
2025-12-25 09:36:40 +0530

    constructor

---
+++ Github450Q/constructor.js
@@ -0,0 +1,12 @@
+class Test{
+    constructor(name){
+       this.name = name;
+    }
+    // constructor(name,age){      //a class may only have one constructor
+    //    this.age = age;
+    //    this.name = name;
+    // }
+}
+
+const obj = new Test("preet")
+console.log(obj);
