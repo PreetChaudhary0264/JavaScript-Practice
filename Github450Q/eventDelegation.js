84ae4caa4f5257972bc223a5bf8bf6be357c9503
Preet Kaler <preetsingh02064@gmail.com>
2025-12-25 22:43:06 +0530

    co

--- Github450Q/eventDelegation.js
+++ Github450Q/eventDelegation.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('ul');
+// const container = document.querySelector('ul');
 
 {/* <ul>
     <li id="1">hi</li>
@@ -6,11 +6,11 @@ const container = document.querySelector('ul');
     <li id="3">hi</li>
 </ul> */}
 
-container.addEventListener("click",(e)=>{
-    if(e.target.tagName === "LI"){           //tagName html ka tag return krta hai uppercase me
-        console.log(`li ${FileList.dataset.id} is clicked`);  
-    }
-})
+// container.addEventListener("click",(e)=>{
+//     if(e.target.tagName === "LI"){           //tagName html ka tag return krta hai uppercase me
+//         console.log(`li ${FileList.dataset.id} is clicked`);  
+//     }
+// })
 
 {/* <div>
     <button id="1"></button>
@@ -18,14 +18,59 @@ container.addEventListener("click",(e)=>{
     <button id="1"></button>
 </div> */}
 
-const arr = [1,2,3,4,5]
-const arr2 = [1,2,3,4,5]
-const arr3 = [1,2,3,4,5]
-const ar4 = [1,2,3,4,5]
-const arr5 = [1,2,3,4,5]
+// const arr = [1,2,3,4,5]
+// const arr2 = [1,2,3,4,5]
+// const arr3 = [1,2,3,4,5]
+// const ar4 = [1,2,3,4,5]
+// const arr5 = [1,2,3,4,5]
 
-const con = document.querySelector('div')
-con.addEventListener("click",(e)=>{
-    const button = e.target.closest("botton")
-    console.log(button.dataset.id);
-})
+// const con = document.querySelector('div')
+// con.addEventListener("click",(e)=>{
+//     const button = e.target.closest("botton")
+//     console.log(button.dataset.id);
+// })
+
+function print(a,...nums){
+    console.log(b);
+}
+
+
+function one(){
+    console.log("This is first function");
+    
+}
+function one(){
+    console.log("This is second function");
+    
+}
+one();
+
+// function loadFilesDynamically(fileName,fileType){
+//     let file;
+//     if(fileType === "css"){
+//         file = document.createElement("link")
+//         file.setAttribute("type","css")
+//         file.setAttribute("href",fileName)
+//     }else if(fileType === "js"){
+//         file = document.createElement("script")
+//         file.setAttribute("type","javascript")
+//         file.setAttribute("src",fileName)
+//     }
+//     if( typeof fileType != undefined){
+//         document.getElementsByTagName("head")[0].appendChild(file);
+//     }
+// }
+
+
+for(var i = 0; i < 4; i++){
+    ;((x)=>{
+        setTimeout(()=> console.log(x))
+    })(i)
+}
+
+console.log(~~3.67);
+
+const arr = [2,3,4]
+var odd = (element) => element % 2 !== 0;
+console.log(arr.some(odd));
+
